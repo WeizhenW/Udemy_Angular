1d306560d4736a4dab2dee01e1fec56b4cf4d60c
Weizhen Wang <wangwzhen@gmail.com>
2021-02-24 10:06:18 -0600

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    template: `
+        <p>The first server component</p>
+        <p>The second server component</p>
+    `
+})
+export class ServerComponent {
+
+}
